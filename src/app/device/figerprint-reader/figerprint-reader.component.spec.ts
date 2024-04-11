1a778e26609d12aadfb4419d433ecb7ac4519371
Aldo Castillo <aldo.castillo@adviters.com>
2024-04-11 12:45:44 -0300

    Fingerprint

---
+++ src/app/device/figerprint-reader/figerprint-reader.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FigerprintReaderComponent } from './figerprint-reader.component';
+
+describe('FigerprintReaderComponent', () => {
+  let component: FigerprintReaderComponent;
+  let fixture: ComponentFixture<FigerprintReaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FigerprintReaderComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(FigerprintReaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
