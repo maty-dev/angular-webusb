f2d63dde492433437ba1e6208db93f0565d1db46
Aldo Castillo <aldo.castillo@adviters.com>
2024-04-12 12:43:28 -0300

    Barcacode Scan. Observable. Uni & Bi.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,19 @@
 import { Routes } from '@angular/router';
-import { FingerprintReader } from '@digitalpersona/devices';
 
 export const routes: Routes = [
     {
         path: 'fingerprint-reader',
-        title: 'Fingerprint Reader',
+        title: 'Fingerprint Reader | Owner - DigitalPersona',
         loadComponent: () => import('./device/figerprint-reader/figerprint-reader.component').then( m => m.FigerprintReaderComponent ),
     },
     {
         path: 'thermal-printer',
-        title: 'Thermal Printer',
+        title: 'Thermal Printer | Generic',
         loadComponent: () => import('./device/thermal-printer/thermal-printer.component').then( m => m.ThermalPrinterComponent ),
     },
     {
         path: 'barcacode-scan',
-        title: 'Barcacode Scan',
+        title: 'Barcacode Scan | Generic (Uni & Bidirectional)',
         loadComponent: () => import('./device/barcode-scan/barcode-scan.component').then( m => m.BarcodeScanComponent ),
     },
 
@@ -27,5 +26,5 @@ export const routes: Routes = [
         path: '**',
         redirectTo: 'fingerprint-reader',
         pathMatch: 'full'
-    },   
+    } 
 ];
